b710d9f5bd8f5a47ced92f23ae369212615f0999
Glenn Chiang <glennchiang24@gmail.com>
2023-06-19 16:21:41 +0800

    Added profile stats

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@
 import { useEffect, useState } from 'react';
 import Home from './views/Home/home.jsx';
 import MyBooks from './views/MyBooks/myBooks.jsx';
+import Profile from './views/Profile/Profile.jsx';
 import Banner from './components/Banner/Banner.jsx';
-import Navbar from './components/Navbar/navbar.jsx';
+import Navbar from './components/Navbar/Navbar.jsx';
 
 export default function App() {
   const [view, setView] = useState('home');
 
-  const currentPage = view === 'home' ? <Home/> : <MyBooks/>
+  const currentPage = view === 'home' ? <Home/> : view === 'myBooks' ? <MyBooks/> : <Profile/>
 
   useEffect(() => {
     if (localStorage.getItem('myBooks')) {
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,16 +4,16 @@ import styles from './Navbar.module.css'
 export default function Navbar({ handleClick }) {
   return (
     <ul className={styles.navbar}>
-      <li onClick={() => {handleClick('home')}}>
+      <li onClick={() => handleClick('home')}>
         Home
       </li>
-      <li onClick={() => {handleClick('browse')}}>
+      <li onClick={() => handleClick('browse')}>
         Browse
       </li>
-      <li onClick={() => {handleClick('myBooks')}}>
+      <li onClick={() => handleClick('myBooks')}>
         My Books
       </li>
-      <li>
+      <li onClick={() => handleClick('profile')}>
         Profile
       </li>
     </ul>
