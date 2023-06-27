046549a2c778b4eb0584d4c402c298dd2b8bd51e
Glenn Chiang <glennchiang24@gmail.com>
2023-06-27 09:49:03 +0800

    Committing history to new branch 'routing' 'Routing' branch will be used to attempt to implement react-router

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,34 @@
 /* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom'
 import { auth } from '../../firebase'
 import styles from './Navbar.module.css'
 
-export default function Navbar({ currentView, handleClick }) {
+export default function Navbar() {
+  const currentView = 'myBooks';
   return (
-    <ul className={styles.navbar}>
-      <li className={currentView === 'search' ? styles.underlined : undefined} onClick={() => handleClick('search')}>
-        Search
-      </li>
-      <li className={currentView === 'myShelves' ? styles.underlined : undefined}  onClick={() => handleClick('myShelves')}>
-        My Shelves
-      </li>
-      <li className={currentView === 'myBooks' ? styles.underlined : undefined} onClick={() => handleClick('myBooks')}>
-        My Books
-      </li>
-      <li className={currentView === 'profile' ? styles.underlined : undefined} onClick={() => handleClick('profile')}>
-        Profile
-      </li>
-      { auth.currentUser ?
-        <li className={currentView === 'signOut' ? styles.underlined : undefined} onClick={() => handleClick('signOut')}>
-          Sign Out
-        </li> :
-        <li className={(currentView === 'signIn' || currentView === 'signUp') ? styles.underlined : undefined} onClick={() => handleClick('signIn')}>
-          Sign In
+    <nav>
+      <ul className={styles.navbar}>
+        <li className={currentView === 'search' ? styles.underlined : undefined} >
+          <Link to='search'>Search</Link>
         </li>
-      }
-    </ul>
+        <li className={currentView === 'myShelves' ? styles.underlined : undefined}  >
+          <Link to='myShelves'>My Shelves</Link>
+        </li>
+        <li className={currentView === 'myBooks' ? styles.underlined : undefined} >
+          <Link to='myBooks'>My Books</Link>
+        </li>
+        <li className={currentView === 'profile' ? styles.underlined : undefined} >
+          <Link to='profile'>Profile</Link>
+        </li>
+        { auth.currentUser ?
+          <li className={currentView === 'signOut' ? styles.underlined : undefined} >
+            <Link to='signOut'>Sign Out</Link>
+          </li> :
+          <li className={(currentView === 'signIn' || currentView === 'signUp') ? styles.underlined : undefined} >
+            <Link to='signIn'>Sign In</Link>
+          </li>
+        }
+      </ul>
+    </nav>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Root from './Root.jsx'
+import Search from './routes/Search/Search'
+import MyShelves from './routes/MyShelves/MyShelves'
+import MyBooks from './routes/MyBooks/myBooks'
+import Profile from './routes/Profile/Profile'
+import SignIn from './routes/SignIn/SignIn'
+import SignUp from './routes/SignUp/SignUp'
+import SignOut from './routes/SignOut/SignOut'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root/>,
+    children: [
+      {
+        path: 'search',
+        element: <Search/>
+      },
+      {
+        path: 'myShelves',
+        element: <MyShelves/>
+      },
+      {
+        path: 'myBooks',
+        element: <MyBooks/>
+      },
+      {
+        path: 'profile',
+        element: <Profile/>
+      },
+      {
+        path: 'signIn',
+        element: <SignIn/>
+      },
+      {
+        path: 'signUp',
+        element: <SignUp/>
+      },
+      {
+        path: 'signOut',
+        element: <SignOut/>
+      }
+    ]
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
